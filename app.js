2b84e93e8141debcbf8ae3d3e9974985d5e80aa6
wayne1098 <wayne10pnd@gmail.com>
2022-08-19 23:13:37 +0700

    edit done 19.d

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const path= require('path');
 const app = express();
-// const productRouter = require('./app/product/routes');
+const productRouter = require('./app/product/routes');
 const productRouterV2 = require('./app/productv2/routes');
 const logger = require ('morgan');
 const port = process.env.PORT || 8080;
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
-// app.use('/api/v1', productRouter);
+app.use('/api/v1', productRouter);
 app.use('/api/v2', productRouterV2);
 app.use((req, res, next) => {
     res.status(404);
@@ -23,4 +23,4 @@ app.use((req, res, next) => {
     })
 })
 
-app.listen(port, () => console.log(`Server: http://localhost:${port}/api/v2/product`));
+app.listen(port, () => console.log(`Server: http://localhost:${port}`));
